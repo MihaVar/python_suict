e6f114a8127b888f90f7966e33f8d049fa6543db
MihaVar <mykhailvarych@gmail.com>
2025-09-26 08:40:42 +0300

    feat: implement student finder class methods in main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from CsvReader import CsvReader
 from CsvToJsonConverter import CsvToJsonConverter
 from JsonReader import JsonReader
+from StudentFinder import StudentFinder
 
 if __name__ == "__main__":
     csv_url = "https://informer.com.ua/dut/python/import/st_gt.csv"
@@ -16,3 +17,10 @@ if __name__ == "__main__":
 
     json_reader = JsonReader()
     json_reader.display_data(json_path)
+
+    finder = StudentFinder(json_reader)
+    finder.load_data(json_path)
+    print('Введіть прізвище студента: ')
+    surname_to_find = input()
+    found_students = finder.find_students_by_surname(surname_to_find)
+    finder.display_students_info(found_students)
