973eba405efcbaed77f1f912b709c37e58f9f550
MihaVar <mykhailvarych@gmail.com>
2025-09-26 08:37:45 +0300

    feat: implement json reader in main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from CsvReader import CsvReader
 from CsvToJsonConverter import CsvToJsonConverter
+from JsonReader import JsonReader
 
 if __name__ == "__main__":
     csv_url = "https://informer.com.ua/dut/python/import/st_gt.csv"
@@ -12,3 +13,6 @@ if __name__ == "__main__":
 
     converter = CsvToJsonConverter()
     converter.read_and_convert(csv_url, json_path)
+
+    json_reader = JsonReader()
+    json_reader.display_data(json_path)
