40cafc8adcb85a4cdb04d6a0a396802ba17925ed
MihaVar <mykhailvarych@gmail.com>
2025-10-02 18:40:23 +0300

    feat: create real and desired performance classes

--- performance.py
+++ performance.py
@@ -15,3 +15,23 @@ class AcademicPerformance(ABC):
     @abstractmethod
     def calculate_average_score(self) -> float:
         pass
+
+class RealPerformance(AcademicPerformance):
+
+    def calculate_average_score(self) -> float:
+        if not self._scores:
+            return 0.0
+        return sum(self._scores) / len(self._scores)
+
+
+class DesiredPerformance(AcademicPerformance):
+
+    def __init__(self, subjects: list[str], desired_scores: list[int], desired_avg_score: float):
+        super().__init__(subjects, desired_scores)
+        self.__desired_avg_score = desired_avg_score
+
+    def calculate_average_score(self) -> float:
+        return self.__desired_avg_score
+
+    def get_desired_scores(self) -> list[int]:
+        return self._scores
