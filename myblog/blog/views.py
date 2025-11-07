ddb03d5f91ecb2b04451554d09bc3fa1aac00758
MihaVar <mykhailvarych@gmail.com>
2025-11-07 16:54:39 +0200

    feat: add view template function

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,7 +1,7 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from myblog.blog.models import Post
+from .models import Post
 
 
 def post_list(request):
@@ -9,9 +9,5 @@ def post_list(request):
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404('Post not found')
-
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
     return render(request, 'blog/post/detail.html', {'post': post})
