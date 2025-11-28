04fe727ec265317b10cf2d2f449ea6bba2cdb6a7
MihaVar <mykhailvarych@gmail.com>
2025-11-28 10:22:54 +0200

    feat: add search

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,9 +1,10 @@
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db.models import Count
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, SearchForm
 
 from .models import Post, Comment
 from .forms import CommentForm
@@ -86,3 +87,26 @@ def post_comment(request, post_id):
         comment.save()
     return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
 
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                search=SearchVector('title', 'body'),
+            ).filter(search=query)
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
