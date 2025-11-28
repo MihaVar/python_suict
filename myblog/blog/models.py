5918971adb7132579c277bb24889388cc789dca9
MihaVar <mykhailvarych@gmail.com>
2025-11-28 08:49:16 +0200

    feat: adapt posts for tag view

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -21,7 +21,7 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    tags = TaggableManager
+    tags = TaggableManager()
     status = models.CharField(
         max_length=2,
         choices=Status.choices,
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -9,23 +9,31 @@ from .forms import CommentForm
 from django.views.decorators.http import require_POST
 
 
+from taggit.models import Tag
+
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
 
-def post_list(request):
-    posts_list = Post.published.all()
-    paginator = Paginator(posts_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        tag_slug = self.kwargs.get('tag_slug')
+
+        if tag_slug:
+            self.tag = get_object_or_404(Tag, slug=tag_slug)
+            qs = qs.filter(tags__in=[self.tag])
+        else:
+            self.tag = None
+
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tag'] = self.tag
+        return context
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
